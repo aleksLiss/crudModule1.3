b97955bef436bfea6d18f5c4328ed652c8a7bdc0
aleksLis <darklisik.darklisik@yandex.ru>
2024-01-07 21:02:03 +0300

    created class LabelView

--- src/main/java/org/AleksLis/CrudApp/view/LabelView.java
+++ src/main/java/org/AleksLis/CrudApp/view/LabelView.java
@@ -1,4 +1,128 @@
 package org.AleksLis.CrudApp.view;
 
+import org.AleksLis.CrudApp.controller.LabelController;
+import org.AleksLis.CrudApp.controller.PostController;
+import org.AleksLis.CrudApp.model.Label;
+import org.AleksLis.CrudApp.model.Post;
+import org.AleksLis.CrudApp.systemMessages.SystemMessages;
+
+import java.util.List;
+import java.util.Scanner;
+
 public class LabelView {
+
+    private LabelController labelController;
+    public static final String INPUT_ID = "Input id: ";
+    public static final String INPUT_NAME = "Input name: ";
+    public static final String ACTION_ON_LABELS = "Choose action on labels: ";
+    public static final String MENU =
+            "1. Create new label\n" +
+                    "2. Get label by id\n" +
+                    "3. Get all labels\n" +
+                    "4. Update label by id\n" +
+                    "5. Delete label by id\n" +
+                    "6. Exit\n";
+
+    public void actionsOnLabels() {
+        try {
+            System.out.println(ACTION_ON_LABELS);
+            System.out.println(MENU);
+            int choose = new Scanner(System.in).nextInt();
+            switch (choose) {
+                case 1:
+                    saveLabel();
+                    break;
+                case 2:
+                    getLabel();
+                    break;
+                case 3:
+                    getAllLabels();
+                    break;
+                case 4:
+                    updateLabel();
+                    break;
+                case 5:
+                    deleteLabel();
+                    break;
+                case 6:
+                    break;
+                default:
+                    System.out.println(SystemMessages.OPERATION_FAILED);
+            }
+        } catch (Exception e) {
+            System.out.println(SystemMessages.OPERATION_FAILED);
+        }
+
+    }
+
+    public void getLabel() {
+        try {
+            System.out.println(INPUT_ID);
+            long id = new Scanner(System.in).nextInt();
+            Label label = labelController.getById(id);
+            System.out.println("Id: " + label.getId());
+            System.out.println("Name: " + label.getName());
+            System.out.println("Status: " + label.getLabelStatus());
+        } catch (Exception e) {
+            System.out.println(SystemMessages.OPERATION_FAILED);
+        }
+    }
+
+    ;
+
+    public void getAllLabels() {
+        try {
+            List<Label> labelList = labelController.getAll();
+            for (Label label : labelList) {
+                System.out.println("Id: " + label.getId());
+                System.out.println("Name: " + label.getName());
+                System.out.println("Status: " + label.getLabelStatus());
+            }
+        } catch (Exception e) {
+            System.out.println(SystemMessages.OPERATION_FAILED);
+        }
+    };
+
+    public void saveLabel() {
+        try {
+            System.out.println(INPUT_ID);
+            long id = new Scanner(System.in).nextInt();
+            System.out.println(INPUT_NAME);
+            String name = new Scanner(System.in).next();
+            labelController.saveLabel(new Label(id, name));
+            System.out.println(SystemMessages.OPERATION_SUCCESS);
+        } catch (Exception e) {
+            System.out.println(SystemMessages.OPERATION_FAILED);
+        }
+    };
+
+    public void updateLabel() {
+        try {
+            System.out.println(INPUT_ID);
+            long id = new Scanner(System.in).nextInt();
+            Label label = labelController.getById(id);
+            System.out.println(INPUT_NAME);
+            String name = new Scanner(System.in).next();
+            label.setName(name);
+            labelController.updateLabel(label);
+            System.out.println(SystemMessages.OPERATION_SUCCESS);
+        } catch (Exception e) {
+            System.out.println(SystemMessages.OPERATION_FAILED);
+        }
+    };
+
+    public void deleteLabel() {
+        try {
+            System.out.println(INPUT_ID);
+            long id = new Scanner(System.in).nextInt();
+            labelController.deleteById(id);
+            System.out.println(SystemMessages.OPERATION_SUCCESS);
+        } catch (Exception e) {
+            System.out.println(SystemMessages.OPERATION_FAILED);
+        }
+    }
+
+
+
+
 }
